ed2a80628d6510768541bb8a2e48409d137836e9
Thanh Tan <tanden1357@gmail.com>
2026-01-07 21:59:58 +0700

    update controller and routers

---
+++ src/index.js
@@ -0,0 +1,52 @@
+const express = require("express")
+const path = require("path")
+const morgan = require("morgan")
+const { engine } = require("express-handlebars") // ← import đúng
+
+const app = express()
+const port = 3000
+const route = require('./routes') // tu tim file index
+
+
+
+// HTTP logger
+app.use(morgan("combined"))
+app.use(express.static(path.join(__dirname, "public")))
+// sử dụng được file scss file tĩnh
+// Template engine setup
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+console.log("Static path:", path.join(__dirname, "public"))
+app.engine("hbs", engine({ extname: ".hbs" })) // ← đặt ext là .hbs
+app.set("view engine", "hbs") // ← view engine là hbs tu tim den file hbs
+app.set("views", path.join(__dirname, "resources\\views"))
+
+// debug path
+console.log("Views path:", path.join(__dirname, "resources\\views"))
+
+// app.get("/", (req, res) => {
+//   return res.send(`
+//     <h1>Hello tat ca moi nguoi</h1>
+//     `)
+// })
+// app.get("/tin-tuc", (req, res) => {
+//   res.render("home") // render file home.hbs
+// })
+
+// app.get("/news", (req, res) => {
+//   res.render("news") // render file home.hbs
+//   console.log(req.query.q)
+// })
+
+// app.get("/search", (req, res) => {
+//   console.log(req.query)
+//   res.render("search")
+// })
+// app.post("/search", (req, res) => {
+//   console.log(req.body) // sử dụng vói post là body
+//   res.send("")
+// })
+route(app);
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
---
+++ src/routes/index.js
@@ -0,0 +1,31 @@
+const newsRouter = require("./news")
+const siteRouter = require('./site')
+function route(app) {
+  app.use("/news", newsRouter)
+  
+  app.use('/', siteRouter)
+//   app.get("/", (req, res) => {
+//     return res.send(`
+//     <h1>Hello tat ca moi nguoi</h1>
+//     `)
+//   })
+//   app.get("/tin-tuc", (req, res) => {
+//     res.render("home") // render file home.hbs
+//   })
+
+  //   app.get("/news", (req, res) => {
+  //     res.render("news") // render file home.hbs
+  //     console.log(req.query.q)
+  //   })
+
+//   app.get("/search", (req, res) => {
+//     console.log(req.query)
+//     res.render("search")
+//   })
+//   app.post("/search", (req, res) => {
+//     console.log(req.body) // sử dụng vói post là body
+//     res.send("")
+//   })
+}
+
+module.exports = route
